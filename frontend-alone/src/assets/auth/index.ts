214fd0a82e4589003d289fe112a3acf86416ebff
lim58 <dylim7715@gmail.com>
2024-08-01 22:20:44 +0900

    add :: auth 아이콘 추가

---
+++ frontend-alone/src/assets/auth/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as Logo } from "./Logo.svg"
+export { ReactComponent as UserNameIcon } from "./UserNameIcon.svg"
+export { ReactComponent as UserIdIcon } from "./UserIdIcon.svg"
+export { ReactComponent as UserPwIcon } from "./UserPwIcon.svg"
